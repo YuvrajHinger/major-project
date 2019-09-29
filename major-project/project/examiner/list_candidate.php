3e4bf937a493bffe35d5fba72187529593caa096
sumitvaishnav12 <sumit.vaishnav12@gmail.com>
2019-09-29 15:38:22 +0530

    update
    update

--- major-project/project/examiner/list_candidate.php
+++ major-project/project/examiner/list_candidate.php
@@ -18,7 +18,44 @@
                                 <h4><a href="">Candidate List</a></h4>
                             </li>
                         </center>
-		            </ol>        
+		            </ol>
+                    <?php
+    if(isset($_POST['search'])){        
+        $candidate_username= $_POST['by_candidate'];                  
+        $query = "select * from `candidate_login` where `is_deleted` = '0' and (name='$candidate_username')";                
+    }
+    else{
+        $query = "select * from `candidate_login` where `is_deleted` = '0'";
+    }
+    if(isset($_POST['deleteAction']))
+    {
+        $delete_id=$_POST['delete_id'];
+        mysql_query("update `candidate_username` SET `is_deleted`='1' where id='$delete_id'" );    
+        $_SESSION['type']="success";
+        $_SESSION['message']="candidate successfully Deleted";
+        echo "<script>window.location.href='';</script>";
+    } 
+?>
+<div class="row">
+<form action="" method="post">
+        <div class="col-md-3">
+        <select name="by_company" class="form-control select2">
+            <option value="">Search By Name</option>
+            <?php
+                $getData=mysql_query("select distinct name from `candidate_username` where `is_deleted` = '0'");		                
+                while($fetchData=mysql_fetch_array($getData)){
+                    $candidate_username= $fetchData['name'];                      
+                    echo "<option>".$candidate_username."</option>";
+                }                 
+			?>
+        </select>
+    </div>
+    <div class="col-md-3">
+        <button style="background-color:#1b93e1; type="submit" name="search" class="btn btn-success">FILTER</button>
+        <button style="background-color:red; type="submit" name="reset" class="btn btn-success">Reset</button>
+    </div>
+</div>
+</form>        
                     <div class="agile-grids">
                         <div class="agile-tables">
                             <div class="w3l-table-info">
@@ -26,10 +63,10 @@
                                 <table width="100%" id="table" class="table table-bordered table-hover">
                                     <thead>
                                         <tr>
-                                            <th>#</th>
-                                            <th>Username</th> 
-                                            <th>Password</th> 
-                                            <th>Action</th>
+                                            <th style="background-color:#1b93e1;color:white;">#</th>
+                                            <th style="background-color:#1b93e1;color:white;">Username</th> 
+                                            <th style="background-color:#1b93e1;color:white;">Password</th> 
+                                            <th style="background-color:#1b93e1;color:white;">Action</th>
                                         </tr>
                                     </thead>
                                     <tbody>                 <?php $getData=$con->query("select * from candidate_login where status='0'"); $i=0; while($fetchData=$getData->fetch_assoc()){ $i++; $id=$fetchData['candidate_id']; $username=$fetchData['candidate_username']; $password=$fetchData['candidate_password']; ?>
